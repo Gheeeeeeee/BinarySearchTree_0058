273ff7185303d900fef25384dd88b538424606f1
gheeeeeeee <gheoputra55@gmail.com>
2024-06-14 14:06:09 +0700

    mengisi member di dalam aksed modifier public serta membuat constructornya

--- BinarySearchTree_0058/BinarySearchTree_0058.cpp
+++ BinarySearchTree_0058/BinarySearchTree_0058.cpp
@@ -5,7 +5,17 @@ using namespace std;
 
 class Node {
 public:
-
+    string info;
+    Node* leftchild;
+    Node* rightchild;
+    
+    //constructor for the node class
+    Node(string i, Node* l, Node* r)
+    {
+        info = i;
+        leftchild = l;
+        rightchild = r;
+    }
 };
 
 int main()
